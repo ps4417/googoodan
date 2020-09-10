b3415d8a928f724548de54b61154fb63df9b2e87
samsung <samsung@DESKTOP-146TQ1E>
2020-09-10 20:55:43 +0900

    메소드를 이용한 구구단 만들기(함수 두개를 만들고 for문을 이용해 구현)

--- src/googoodan/gugudan_method.java
+++ src/googoodan/gugudan_method.java
@@ -2,29 +2,28 @@ package googoodan;
 
 public class gugudan_method {
 
+	// 단을 의미하는 calculate 함수
 	public static int[] calculate(int times) {
-		int[]result = new int[9];
-		for(int i=0;i<result.length;i++) {
-			result[i] = times *(i+1); 
+		int[] result = new int[9];
+		for (int i = 0; i < result.length; i++) {
+			result[i] = times * (i + 1);
 		}
 		return result;
 	}
-	
-	
-			public static void main(String[] args) {
-				int[]result = calculate(2);
-				
-				for(int i=0;i<result.length;i++) {
-					System.out.println(result[i]);
-				}
-			}
-		
 
+	// 단에 곱해지는 수들을 출력하는 print 함수
+	public static void print(int[] result) {
+		for (int i = 0; i < result.length; i++) {
+			System.out.println(result[i]);
+		}
+	}
 
+	public static void main(String[] args) {
+		for (int i = 2; i < 10; i++) {
+			int[] result = calculate(i);
+			print(result);
+		}
+
+	}
 
 }
-
-
-
-
-
